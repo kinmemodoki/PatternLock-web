003aefdb30b718bc1086f7d65e9e3268384bc990
kinmemodoki <kinmemodoki@gmail.com>
2017-10-10 20:54:17 +0900

    demoのIDせってい

--- scripts/demo.js
+++ scripts/demo.js
@@ -35,12 +35,26 @@ regist
 auth
 （確認）自動送信（送信）：（わすれた）->registでリロード
 */
+function create_privateid( n ){
+      var CODE_TABLE = "0123456789"
+          + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+          + "abcdefghijklmnopqrstuvwxyz";
+      var r = "";
+      for (var i = 0, k = CODE_TABLE.length; i < n; i++){
+          r += CODE_TABLE.charAt(Math.floor(k * Math.random()));
+      }
+      return r;
+    }
 
 const pageCtr = (function(){
   var context = getUserData();
   var user = context.player ? JSON.parse(context.player) : {};
   var isConfirm = 0;
   var rank,tmpPattern,pattern;
+  if(!user.id){
+    user.id = create_privateid(24);
+    docCookies.setItem("player",JSON.stringify(user));
+  }
   console.log(user);
   if(!user.prepattern && getUrlVars().status!="regist"){
     window.location.replace("/demo.html?status=regist")
