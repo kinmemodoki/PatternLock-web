f409f907fceb8584d6d7a10b3221e62bfa193383
kinmemodoki <kinmemodoki@gmail.com>
2018-01-27 14:42:08 +0900

    toppage修正

--- scripts/toppage.js
+++ scripts/toppage.js
@@ -1,3 +1,5 @@
 var rand = Math.floor( Math.random()*5)+1;
 
 document.getElementById("bg-image"+rand).style.display = "block";
+
+alert("本アプリはスマートフォンに最適化されています.\nPCの場合はブラウザウィンドウを縦長にしてください．\n(640x960pixel推奨)");
